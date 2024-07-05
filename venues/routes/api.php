768d9f758b147747b7811e232b2c899e085d1712
nemm22 <nemanja.micovic11@gmail.com>
2024-07-05 19:00:06 +0200

    kontroleri_rute_rezervacije

--- venues/routes/api.php
+++ venues/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/reservations', [ReservationController::class, 'index']);
+Route::get('/reservations/{id}', [ReservationController::class, 'show']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
