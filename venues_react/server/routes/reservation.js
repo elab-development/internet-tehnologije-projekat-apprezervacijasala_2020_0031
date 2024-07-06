cf30d5f2191c40503117003c8b5129a2403816d7
danicamladenovic <danicamladenovic019@gmail.com>
2024-07-06 15:43:57 +0200

    server-reservation

---
+++ venues_react/server/routes/reservation.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import * as reservationController from '../controllers/reservationController.js';
+
+const router = Router();
+
+// POST Endpoints
+router.route('/').post(reservationController.createReservation);
+
+// GET Endpoints
+router.route('/').get(reservationController.getReservations);
+router.route('/:id').get(reservationController.getReservation);
+
+// PUT Endpoints
+router.route('/:id').put(reservationController.updateReservation);
+
+// DELETE Endpoints
+router.route('/:id').delete(reservationController.deleteReservation);
+
+export default router;
