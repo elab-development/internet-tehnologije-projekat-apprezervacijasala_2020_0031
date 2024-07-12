1833275097bc73b2d52e7f7fb633aa8aa1ee1edc
nemm22 <nemanja.micovic11@gmail.com>
2024-07-12 15:17:03 +0200

    proj_cl_utils

---
+++ venues-reservation/client/src/utils/constants.js
@@ -0,0 +1,23 @@
+export const slots = [
+    {
+      time: '09:00',
+      available: true,
+    },
+    {
+      time: '12:00',
+      available: true,
+    },
+    {
+      time: '15:00',
+      available: true,
+    },
+    {
+      time: '18:00',
+      available: true,
+    },
+    {
+      time: '21:00',
+      available: true,
+    },
+  ];
+  
